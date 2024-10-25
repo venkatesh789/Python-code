e3241b353bba19ab43a1d6f70e6a868053cf3332
venkatesh789 <venkateshbaviskar@gmail.com>
2024-10-25 16:18:11 +0530

    Hello.py contains basic example

---
+++ hello.py
@@ -0,0 +1,17 @@
+fruit = 'apple'
+
+
+first3letters ='abc'
+first_three_letters ='venkatesh'
+
+
+double = "She said, \"Thats's great testing apple! \""
+
+single = 'She said, "That\'s a greate apple'
+
+
+char = fruit [0]
+print (char)
+
+print('Venkatesh')
+
